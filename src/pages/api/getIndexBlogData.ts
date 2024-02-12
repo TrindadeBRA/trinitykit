c05369363e66c09507c858389d04c1eaa99453b0
Lucas Trindade <trindadebra@gmail.com>
2024-02-12 18:17:59 -0300

    Blog Init

---
+++ src/pages/api/getIndexBlogData.ts
@@ -0,0 +1,17 @@
+// export const getIndexBlogData = async () => {
+//   try {
+//     const response = await fetch(`${process.env.WORDPRESS_URL}/wp-json/trinitykit/v1/last-posts`); // ajuste per_page conforme necessário
+    
+//     if (!response.ok) {
+//       throw new Error('Failed to fetch post slugs from API');
+//     }
+    
+//     const postData = await response.json();
+
+//     return postData;
+
+//   } catch (error) {
+//     console.error('Error fetching post slugs:', error);
+//     throw error;
+//   }
+// };
