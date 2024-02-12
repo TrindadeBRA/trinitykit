a2ffcd5bcb9bbd92e50ac2320e036eca87260807
Lucas Trindade <trindadebra@gmail.com>
2024-02-12 19:25:49 -0300

    Blog added component

--- src/components/IndexBlog/index.tsx
+++ src/components/IndexBlog/index.tsx
@@ -1,6 +1,6 @@
 // import { IndexBlogDataType } from "@/pages/api/getIndexBlogData";
 
-export default function IndexBlog({indexBlogData}: any) {
+export default function IndexBlog({ indexBlogData }: any) {
 
   console.log(indexBlogData)
 
@@ -14,11 +14,11 @@ export default function IndexBlog({indexBlogData}: any) {
           </p>
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {Object.values(indexBlogData).map((post:any) => (
+          {Object.values(indexBlogData).map((post: any) => (
             <article key={post.id} className="flex flex-col items-start justify-between">
               <div className="relative w-full">
                 <img
-                  src={post.imageUrl ?? "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80"}
+                  src={"https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80"}
                   alt=""
                   className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
                 />
@@ -26,35 +26,35 @@ export default function IndexBlog({indexBlogData}: any) {
               </div>
               <div className="max-w-xl">
                 <div className="mt-8 flex items-center gap-x-4 text-xs">
-                  <time dateTime={post.datetime ?? "#"} className="text-gray-500">
+                  <time dateTime={"post.datetime"} className="text-gray-500">
                     {post.date}
                   </time>
                   <a
                     href={"#"}
                     className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
                   >
-                    {"#"}
+                    {"post.category.title"}
                   </a>
                 </div>
                 <div className="group relative">
                   <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                    <a href={"#"}>
+                    <a href={post.href}>
                       <span className="absolute inset-0" />
                       {post.title}
                     </a>
                   </h3>
-                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{"#"}</p>
+                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{"post.description"}</p>
                 </div>
                 <div className="relative mt-8 flex items-center gap-x-4">
-                  <img src={"https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"} alt="" className="h-10 w-10 rounded-full bg-gray-100" />
+                  <img src={"https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80"} alt="" className="h-10 w-10 rounded-full bg-gray-100" />
                   <div className="text-sm leading-6">
                     <p className="font-semibold text-gray-900">
                       <a href={"#"}>
                         <span className="absolute inset-0" />
-                        {"#"}
+                        {"post.author.name"}
                       </a>
                     </p>
-                    <p className="text-gray-600">{"#"}</p>
+                    <p className="text-gray-600">{"post.author.role"}</p>
                   </div>
                 </div>
               </div>
--- src/pages/api/getIndexBlogData.ts
+++ src/pages/api/getIndexBlogData.ts
@@ -1,17 +1,17 @@
-// export const getIndexBlogData = async () => {
-//   try {
-//     const response = await fetch(`${process.env.WORDPRESS_URL}/wp-json/trinitykit/v1/last-posts`); // ajuste per_page conforme necessário
+export const getIndexBlogData = async () => {
+  try {
+    const response = await fetch(`${process.env.WORDPRESS_URL}/wp-json/trinitykit/v1/latest-posts`);
     
-//     if (!response.ok) {
-//       throw new Error('Failed to fetch post slugs from API');
-//     }
+    if (!response.ok) {
+      throw new Error('Failed to fetch post slugs from API');
+    }
     
-//     const postData = await response.json();
+    const postData = await response.json();
 
-//     return postData;
+    return postData;
 
-//   } catch (error) {
-//     console.error('Error fetching post slugs:', error);
-//     throw error;
-//   }
-// };
+  } catch (error) {
+    console.error('Error fetching post slugs:', error);
+    throw error;
+  }
+};
