9d9e2817f96ce85d86c199233bc06f0d3e4f71b7
Lucas Trindade <trindadebra@gmail.com>
2024-03-03 19:31:41 -0300

    update componente bg color

--- src/components/BlogPostPage/Content/index.tsx
+++ src/components/BlogPostPage/Content/index.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 
 export default function BlogPostContent({ post }: any) {
   return (
-    <div className="container mx-auto my-8 p-4 text-[#fff]" dangerouslySetInnerHTML={{ __html: post.content.rendered }} id="blog-content">
+    <div className="bg-[#030712]">
+      <div className="container mx-auto my-8 p-4 text-[#fff]" dangerouslySetInnerHTML={{ __html: post.content.rendered }} id="blog-content">
+      </div>
     </div>
   )
 }
