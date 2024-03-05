871964312369ea3312328053b6338097f070ca62
Lucas Trindade <trindadebra@gmail.com>
2024-03-05 16:57:55 -0300

    new layout header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-BR" className='scroll-smooth'>
-      <Head />
+      <Head>
+        <script src="/js/darkMode.bundle.js" defer></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <script src="/js/app.bundle.js" defer></script>
       </body>
     </Html>
   )
