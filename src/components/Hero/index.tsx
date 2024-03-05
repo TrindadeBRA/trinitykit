cca625c3e6cca63ba919feba7f1b74cb5b17ceaf
Lucas Trindade <trindadebra@gmail.com>
2024-03-05 17:14:56 -0300

    test

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,6 +1,11 @@
 import { HomeDataType } from "@/pages/api/getHomeData";
 
 export default function HeroSection(homeData: HomeDataType) {
+
+  function teste(){
+    return console.log("XXX")
+  }
+
   return (
     <div className="bg-gray-900">
       <div className="relative isolate overflow-hidden">
@@ -31,9 +36,9 @@ export default function HeroSection(homeData: HomeDataType) {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href={homeData?.hero?.cta_url}
-                target="_blank"
+                
                 className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+                onClick={teste}
               >
                 {homeData?.hero?.cta_text}
               </a>
