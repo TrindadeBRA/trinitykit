e076de9d4058421479a2bf0eb0229394f4ab029e
Lucas Trindade <trindadebra@gmail.com>
2024-03-05 22:08:29 -0300

    Fix Hero

--- src/components/HeroSlider/index.tsx
+++ src/components/HeroSlider/index.tsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 import React from "react";
 
 export default function HeroSlider() {
     return (
-        <section className="relative pb-10 pt-24 md:pt-48 h-[100vh]">
+        <section className="relative h-[100vh]">
             <picture className="pointer-events-none absolute left-0 right-0 top-0 bottom-0 -z-10 dark:hidden">
                 <img src="img/gradient.jpg" alt="gradient" className="w-full h-full object-cover" />
             </picture>
@@ -10,10 +11,10 @@ export default function HeroSlider() {
                 <img src="img/gradient_dark.jpg" alt="gradient dark" className="w-full h-full object-cover" />
             </picture>
 
-            <div className="container">
+            <div className="container h-full">
                 <div className="grid h-full items-center gap-4 md:grid-cols-12">
                     
-                    <div className="col-span-6 flex h-full flex-col items-center justify-center py-10 md:items-start md:py-20 xl:col-span-4">
+                    <div className="col-span-6 flex h-full flex-col items-center justify-center pt-[113px] md:items-start xl:col-span-4">
                         <h1 className="mb-6 text-center font-display text-5xl text-jacarta-700 dark:text-white md:text-left lg:text-6xl xl:text-7xl">
                             Desbravando mundos literários!
                         </h1>
@@ -33,7 +34,7 @@ export default function HeroSlider() {
                     {/* Hero image */}
                     <div className="col-span-6 xl:col-span-8">
                         <div className="relative text-center md:pl-8 md:text-right">
-                            <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" className="mt-8 inline-block w-72 rotate-[8deg] sm:w-full lg:w-[24rem] xl:w-[35rem]">
+                            <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" className="mt-8 inline-block w-72 rotate-[8deg] sm:w-[72] xl:w-[35rem]">
                                 <defs>
                                     <clipPath id="clipping" clipPathUnits="userSpaceOnUse">
                                         <path d="M 0, 100 C 0, 17.000000000000004 17.000000000000004, 0 100, 0 S 200, 17.000000000000004 200, 100 183, 200 100, 200 0, 183 0, 100" fill="#9446ED"></path>
@@ -43,7 +44,7 @@ export default function HeroSlider() {
                                     <image href="img/hero/hero.jpg" width="200" height="200" clipPath="url(#clipping)" />
                                 </g>
                             </svg>
-                            <img src="img/hero/3D_elements.png" alt="" className="absolute top-0 animate-fly md:-right-[10%]" />
+                            <Image width={1000} height={1000} src="img/hero/3D_elements.png" alt="" className="absolute top-0 animate-fly md:-right-[10%] " />
                         </div>
                     </div>
                 </div>
