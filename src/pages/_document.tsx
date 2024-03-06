447dc4572fe6a7ad2ed2272170e6d0d1bfcf8143
Lucas Trindade <trindadebra@gmail.com>
2024-03-06 14:22:04 -0300

    Remove theme scripts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,13 +4,13 @@ export default function Document() {
   return (
     <Html lang="pt-BR" className='scroll-smooth'>
       <Head>
-        <script src="/js/darkMode.bundle.js" defer></script>
+        {/* <script src="/js/darkMode.bundle.js" defer></script> */}
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
       </Head>
       <body className="bg-light-base dark:bg-jacarta-900">
         <Main />
         <NextScript />
-        <script src="/js/app.bundle.js" defer></script>
+        {/* <script src="/js/app.bundle.js" defer></script> */}
       </body>
     </Html>
   )
