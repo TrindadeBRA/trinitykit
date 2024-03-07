f2c1cb73652cc5257cbf0cd4aa4293487dbff9e8
Lucas Trindade <trindadebra@gmail.com>
2024-03-07 19:18:50 -0300

    Create sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** 
+ * @type {import('next-sitemap').IConfig} 
+ */// Default code you can customize according to your requirements.
+ module.exports = {
+    siteUrl: 'https://hml.thetrinityweb.com.br',
+    generateRobotsTxt: true, // (optional)
+    // REST CODE READ DOCS  ...
+}
