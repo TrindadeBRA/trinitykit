0c762e98589fec4d517d072a90604c209ba0723c
Lucas Trindade <trindadebra@gmail.com>
2024-03-08 19:15:58 -0300

    update button SEO

--- src/components/NewHeader/index.tsx
+++ src/components/NewHeader/index.tsx
@@ -232,7 +232,7 @@ export default function NewHeader({ menuData, settings }: HeaderProps) {
 
 
               {/* <!-- Dark Mode --> */}
-              <a
+              <button
                 className="js-dark-mode-trigger cursor-pointer group ml-2 flex h-10 w-10 items-center justify-center rounded-full border border-jacarta-200 dark:border-transparent bg-white dark:bg-white/[.15] transition-colors hover:bg-accent focus:bg-accent"
                 onClick={toggleTheme}
               >
@@ -260,7 +260,7 @@ export default function NewHeader({ menuData, settings }: HeaderProps) {
                     d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z"
                   />
                 </svg>
-              </a>
+              </button>
             </div>
           </div>
 
@@ -268,7 +268,7 @@ export default function NewHeader({ menuData, settings }: HeaderProps) {
           <div className="ml-auto flex lg:hidden">
 
             {/* <!-- Dark Mode --> */}
-            <a
+            <button
               className="js-dark-mode-trigger cursor-pointer group ml-2 flex h-10 w-10 items-center justify-center rounded-full border border border-jacarta-200 dark:border-transparent bg-white dark:bg-white/[.15] transition-colors hover:bg-accent focus:bg-accent"
               onClick={toggleTheme}
             >
@@ -296,7 +296,7 @@ export default function NewHeader({ menuData, settings }: HeaderProps) {
                   d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z"
                 />
               </svg>
-            </a>
+            </button>
 
             {/* <!-- Mobile Menu Toggle --> */}
             <button
