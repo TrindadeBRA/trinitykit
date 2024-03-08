aa358c68dcc562f3567b6f7e9206c6050fd3f36c
Lucas Trindade <trindadebra@gmail.com>
2024-03-08 18:35:50 -0300

    Teste Scripts

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
+  experimental: {
+    nextScriptWorkers: true,
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
