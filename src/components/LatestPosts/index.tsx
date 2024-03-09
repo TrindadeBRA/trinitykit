2330e1ecda402f13090a2c6b5b232ed8b7c1e812
Lucas Trindade <trindadebra@gmail.com>
2024-03-09 00:32:56 -0300

    update ADS Block

--- src/components/LatestPosts/index.tsx
+++ src/components/LatestPosts/index.tsx
@@ -71,7 +71,7 @@ export default function LatestPosts({ indexBlogData }: any) {
           ))}
         </div>
 
-        <div className="mt-4">
+        <div className="mt-20">
           <AdBanner
             data-ad-slot="3989446316"
             data-ad-format="auto"
