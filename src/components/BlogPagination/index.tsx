bf279756b78020b4dcaa9ddc52c69bb99a66ea98
Lucas Trindade <trindadebra@gmail.com>
2024-03-09 01:25:24 -0300

    fix css

--- src/components/BlogPagination/index.tsx
+++ src/components/BlogPagination/index.tsx
@@ -9,7 +9,7 @@ export default function BlogPagination({ currentPage, totalPages }: any) {
   const isLastPage = currentPage === totalPages;
   
   return (
-    <div className="bg-white dark:bg-black pb-16">
+    <div className="bg-light-base dark:bg-jacarta-800 pb-16">
       <div className="container flex gap-2 justify-center">
         {!isFirstPage && (
           <Link href={`/blog/page/${currentPage - 1}`} className="rounded-full bg-accent py-2 px-6 text-center font-semibold text-white shadow-accent-volume transition-all hover:bg-accent-dark">
